add5a82071737acfb346a1b2522a6576ac50918c
EmilyLow <emlow@me.com>
2020-08-05 11:59:37 -0500

    Submit works

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,21 +1,45 @@
 import React from 'react';
 
-function TodoForm() {
+class TodoForm extends React.Component {
 
-        
-    return (
-        <div>
-            <p>To Do Form</p>
-            <form>
-                <input 
-                type="text"
-                >
-                </input>
-                <button>Submit</button>
-            </form>
-            <button>Clear Completed</button>
-        </div>
-    )
+    constructor() {
+        super();
+        this.state = {
+            todo: ""
+        };
+    }   
+
+    handleChange = e => {
+        this.setState({todo: e.target.value});
+    };
+
+    //Not quite clear where props are pulling from here. I know I'm passing in addItem, but is the fact that the passed in thing called props just assumed?
+    onSubmit = e => {
+        console.log("submitted");
+        e.preventDefault();
+        this.props.addItem(this.state.todo);
+        this.setState({todo: ""});
+    }
+    render() {
+        return (
+            <div>
+                <p>To Do Form</p>
+                <form onSubmit = {this.onSubmit}>
+                    <input 
+                    type="text"
+                    name="todo"
+                    value={this.state.todo}
+                    onChange={this.handleChange}
+                    >
+                    </input>
+                    <button>Submit</button>
+                </form>
+                <button>Clear Completed</button>
+            </div>
+        )
+
+    }
+    
 }
 
 export default TodoForm;
